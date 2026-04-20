1a584cf847dce08289edaf75bab5ae4a7bcd7e3e
Qianyi Liu <163278648+whslqy@users.noreply.github.com>
2026-04-20 04:07:19 +0000

    edit login

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'genre', 'published_year')
+    list_filter = ('genre', 'published_year')
+    search_fields = ('title', 'author', 'genre')
+    ordering = ('title',)
